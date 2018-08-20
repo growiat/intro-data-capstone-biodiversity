038da0ee5a6254a6c7a0b8259f0475dd5fb3f72c
growiat <36081468+growiat@users.noreply.github.com>
2018-08-20 12:50:06 +0100

    Add files via upload

---
+++ Submission Folder/script4.py
@@ -0,0 +1,14 @@
+baseline = 15
+minimum_detectable_effect = 33
+sample_size_per_variant = 890
+yellowstone_weeks_observing = 890.0/507
+bryce_weeks_observing = 890.0/250
+#clue = percentage of sheep with f&m disease / observation
+#sample size / clue
+
+print bryce_weeks_observing
+gsmn = 890.0/149
+print gsmn
+print yellowstone_weeks_observing
+ynp2 = 890.0/282
+print ynp2
